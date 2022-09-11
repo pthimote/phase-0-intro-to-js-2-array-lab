4b8009c11b83e25bbdbc2131b5bcbfa276890ca2
Paul Thimote <pthimote@gmail.com>
2022-09-10 21:44:21 -0400

    commit tree e35fd850fae8f2c29d0a292698770eff9684611c

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+} 
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+   const SpreadOperator = [...cats, name];
+   return SpreadOperator
+}
+
+function prependCat(name) {
+    const SpreadOperator = [name, ...cats];
+    return SpreadOperator
+}
+
+function removeLastCat(name) {
+    const SpreadOperator = (cats.slice(0, -1));
+    return SpreadOperator
+}
+
+function removeFirstCat() {
+    const SpreadOperator = (cats.slice(1));
+    return SpreadOperator
+}
